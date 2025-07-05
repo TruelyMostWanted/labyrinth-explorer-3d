14437dfb94bd8aeee156a026618f65b7fc2aa87c
TruelyMostWanted <45302427+TruelyMostWanted@users.noreply.github.com>
2025-07-06 00:21:06 +0200

    Created new Behaviours for Ears+Eyes , Added Response Ability, Added CharacterState Component

---
+++ truelymostwanted/godot-project/scripts/game/npc/behaviours/EarsNpcBehaviour3D.cs
@@ -0,0 +1,34 @@
+﻿using Godot;
+using LabyrinthExplorer3D.scripts.game.behaviours;
+using LabyrinthExplorer3D.scripts.game.player;
+
+namespace LabyrinthExplorer3D.scripts.game.npc.behaviours;
+
+[GlobalClass]
+public partial class EarsNpcBehaviour3D : CharacterBehaviour3D
+{
+    [Export] public bool HearsPlayer;
+    [Export] public float DistanceToPlayer;
+    [Export] public double TimeWithAudio;
+    [Export] public double TimeSinceLastAudio;
+    
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        
+        HearsPlayer = OwningPlayer.Ears.IsHearingAnyPlayer();
+        
+        if (HearsPlayer)
+        {
+            DistanceToPlayer = OwningPlayer.GlobalPosition.DistanceTo(OwningPlayer.Ears.PlayersInRange[0].GlobalPosition);
+            TimeWithAudio += delta;
+            TimeSinceLastAudio = 0;       
+        }
+        else
+        {
+            DistanceToPlayer = -1;
+            TimeWithAudio = 0;       
+            TimeSinceLastAudio += delta;       
+        }
+    }
+}
---
+++ truelymostwanted/godot-project/scripts/game/npc/behaviours/EyesNpcBehaviour3D.cs
@@ -0,0 +1,34 @@
+﻿using Godot;
+using Godot.Collections;
+using LabyrinthExplorer3D.scripts.game.behaviours;
+using LabyrinthExplorer3D.scripts.game.player;
+
+namespace LabyrinthExplorer3D.scripts.game.npc.behaviours;
+
+[GlobalClass]
+public partial class EyesNpcBehaviour3D : CharacterBehaviour3D
+{
+    [Export] public bool SeesPlayer;
+    [Export] public float DistanceToPlayer;
+    [Export] public double TimeInSight;
+    [Export] public double TimeSinceLastSeen;
+    
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        var globalPos = PlayerController3D.Instance.CurrentPlayer.GlobalPosition;
+        SeesPlayer = OwningPlayer.Eyes.IsPositionInFrustum(globalPos);
+        if (SeesPlayer)
+        {
+            DistanceToPlayer = OwningPlayer.GlobalPosition.DistanceTo(globalPos);
+            TimeInSight += delta;
+            TimeSinceLastSeen = 0;       
+        }
+        else
+        {
+            DistanceToPlayer = -1;
+            TimeInSight = 0;       
+            TimeSinceLastSeen += delta;       
+        }
+    }
+}
